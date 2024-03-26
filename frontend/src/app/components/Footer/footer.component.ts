b729ff2b284aa2b7dd1243a59d01083a3e40551d
Artyom <Artyom.lymar@bk.ru>
2024-03-26 22:31:45 +0600

    Frontend make footer without response.

--- frontend/src/app/components/Footer/footer.component.ts
+++ frontend/src/app/components/Footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: "Footer",
     standalone: true,
-    imports: [],
+    imports: [RouterLink],
     templateUrl: "./footer.component.html",
     styleUrl: "./footer.component.scss",
 })
