61a8605efe0db6f24dcb5e00e3399344ae6f76c1
Artyom <Artyom.lymar@bk.ru>
2024-04-06 20:44:41 +0600

    Frontend: Made animation for burger-menu

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,11 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef } from "@angular/core";
 import { RouterLink } from "@angular/router";
-import { NgForOf, NgIf } from "@angular/common";
+import { NgClass, NgForOf, NgIf } from "@angular/common";
 
 @Component({
     selector: "app-header",
     standalone: true,
-    imports: [RouterLink, NgIf, NgForOf],
+    imports: [RouterLink, NgIf, NgForOf, NgClass],
     templateUrl: "./header.component.html",
     styleUrl: "./header.component.scss",
 })
@@ -23,18 +23,23 @@ export class HeaderComponent {
 
     showMenu: boolean = false;
 
-    constructor() {
+    constructor(private el: ElementRef) {
         // TODO: Потом получить значение через behaviorSubject.
         this.cartCount = 0;
     }
 
     toggleMenu() {
+        const menu = this.el.nativeElement.querySelector(".menu-burger");
         this.showMenu = !this.showMenu;
 
         if (this.showMenu) {
             document.body.style.overflow = "hidden";
+            menu.style.display = "flex";
         } else {
             document.body.style.overflow = "auto";
+            setTimeout(() => {
+                menu.style.display = "none";
+            }, 400);
         }
     }
 
--- frontend/src/app/components/slider/slider.component.ts
+++ frontend/src/app/components/slider/slider.component.ts
@@ -1,17 +1,150 @@
-import { Component, OnInit } from "@angular/core";
-import { NgForOf } from "@angular/common";
+import { Component, ElementRef, OnInit } from "@angular/core";
+import { NgClass, NgForOf } from "@angular/common";
+import { CatalogService } from "../../services/catalog.service";
+import { Slider } from "../../models/Catalog";
+import { CardComponent } from "../card/card.component";
 
 @Component({
     selector: "app-slider",
     standalone: true,
-    imports: [NgForOf],
+    imports: [NgForOf, CardComponent, NgClass],
+    providers: [CatalogService],
     templateUrl: "./slider.component.html",
     styleUrl: "./slider.component.scss",
 })
 export class SliderComponent implements OnInit {
-    sliderItems: [] = [];
+    // TODO: Данные для теста или можно использовать как промо-товары
+    initSliderItems: Slider[] = [
+        {
+            id: 1,
+            title: "Подобрано для вас id: 1",
+            cost: 12123,
+        },
+        {
+            id: 2,
+            title: "Подобрано для вас id: 1",
+            cost: 12123,
+        },
+        {
+            id: 3,
+            title: "Подобрано для вас id: 1",
+            cost: 12123,
+        },
+        {
+            id: 4,
+            title: "Подобрано для вас id: 1",
+            cost: 12123,
+        },
+        //
+        {
+            id: 1,
+            title: "Подобрано для вас id: 12",
+            cost: 12123,
+        },
+        {
+            id: 2,
+            title: "Подобрано для вас id: 21",
+            cost: 12123,
+        },
+        {
+            id: 3,
+            title: "Подобрано для вас id: 21",
+            cost: 12123,
+        },
+        {
+            id: 4,
+            title: "Подобрано для вас id: 21",
+            cost: 12123,
+        },
+    ];
 
-    constructor() {}
+    sliderIndex = 0; // Текущий индекс
+    sliderItems: Slider[] = [];
 
-    ngOnInit() {}
+    constructor(
+        private catalogService: CatalogService,
+        private el: ElementRef,
+    ) {}
+
+    ngOnInit() {
+        this.catalogService.getProdCarousel().subscribe((data) => {
+            // TODO: Отправлять только ...data
+            this.initSliderItems = [...this.initSliderItems, ...data];
+        });
+        this.updateSlider();
+    }
+
+    /**
+     * Переключение на следующий слайд.
+     */
+    nextSlide() {
+        let nextIndex = this.sliderIndex + 4; // Обновляем индекс
+
+        // Если индекс больше или равен количеству слайдов, то установить индекс в 0
+        if (nextIndex >= this.initSliderItems.length) {
+            this.sliderIndex = 0;
+        } else {
+            this.sliderIndex = nextIndex;
+        }
+
+        this.updateSlider(); // Обновляем слайдер
+    }
+
+    /**
+     * Переключение на предыдущий слайд.
+     */
+    prevSlide() {
+        let prevIndex = this.sliderIndex - 4;
+
+        if (prevIndex < 0) {
+            this.sliderIndex =
+                this.initSliderItems.length -
+                (this.initSliderItems.length % 4 || 4);
+        } else {
+            this.sliderIndex = prevIndex;
+        }
+
+        this.updateSlider();
+    }
+
+    /**
+     * Перерисовка слайдера.
+     */
+    updateSlider() {
+        // Определяем новые элементы для слайдера
+        const items = this.initSliderItems.slice(
+            this.sliderIndex,
+            this.sliderIndex + 4,
+        );
+
+        this.sliderItems = [...items];
+        this.updateNavigation();
+    }
+
+    /**
+     * Обновление кнопок навигации.
+     */
+    updateNavigation() {
+        const navigation = document.querySelectorAll(".navigation__item");
+
+        navigation.forEach((item) => {
+            item.classList.remove("active");
+        });
+        navigation[this.sliderIndex / 4].classList.add("active");
+    }
+
+    /**
+     * Отслеживание нажатия на кнопки навигации.
+     */
+    trackNavigation() {
+        const navigation =
+            this.el.nativeElement.querySelectorAll(".navigation__item");
+
+        navigation.forEach((item: any) => {
+            item.addEventListener("click", () => {
+                this.sliderIndex = +item.getAttribute("data-index");
+                this.updateSlider();
+            });
+        });
+    }
 }
