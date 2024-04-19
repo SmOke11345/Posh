c7012b27f0a3f3db89d240a4f6bd1a52bb7eb03a
Artyom <Artyom.lymar@bk.ru>
2024-04-19 21:09:19 +0600

    Frontend: small changes.

--- frontend/src/app/models/enums/requestUrls.ts
+++ frontend/src/app/models/enums/requestUrls.ts
@@ -1,11 +1,13 @@
-// const host = "https://posh.vercel.app/api";
+// TODO: НЕ ТРОГАТЬ!!!!!!
 const host = "https://posh-st51.onrender.com/api";
+// const host = "http://localhost:3000/api";
 
 export enum Url {
     LOGIN = `${host}/auth/login`,
     REGISTER = `${host}/auth/register`,
     PROFILE = `${host}/users/profile`,
     CATALOG = `${host}/catalogs`,
+    REVIEW = `${host}/reviews`,
     CART = `${host}/cart`,
     FAVORITE = `${host}/favorites`,
 }
